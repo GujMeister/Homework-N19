00d83918578bb69619a8b6b81f8c1aefa17ddf1c
Me <lukagujejiani@gmail.com>
2024-04-19 15:38:43 +0400

    Initial Commit

---
+++ ვებ სერვისებთან კომუნიკაცია/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ვებ სერვისებთან კომუნიკაცია
+//
+//  Created by Luka Gujejiani on 19.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
